6927faafbbe45e5e7b7eae6678e92d0dc821de67
isuhas24 <86099959+isuhas24@users.noreply.github.com>
2023-11-08 22:19:28 +0530

    Add files via upload

---
+++ react-assignments/reactAssignment2-using-css/src/components/App.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import "../style.css";
+import axios from "axios";
+ 
+class App extends Component{
+    state = {
+        users : []
+    }
+
+    componentDidMount(){
+        axios.get("https://reqres.in/api/users?page=1")
+        .then(res => this.setState({users : res.data.data}))
+        .catch(err => console.log("error:",err));
+    }
+
+    render(){
+        return <div>
+                    <h1>USING CSS</h1>
+                    <hr />
+                    <ol>
+                        {this.state.users.map(val =>  <li key={val.id} >{val.first_name} <img width="100px" src={val.avatar} alt=""/></li>  )} 
+                    </ol>
+        </div>
+    }
+}
+ 
+export default App;
